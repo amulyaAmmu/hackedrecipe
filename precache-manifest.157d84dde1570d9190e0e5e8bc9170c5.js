50eb5de18489bc8d394989c9a737680e72396fa4
Shubham Chopade <shubhamchopade10@gmail.com>
2020-08-09 22:21:48 +0530

    Updates

---
+++ precache-manifest.157d84dde1570d9190e0e5e8bc9170c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176b9ed83cfd4b2db681e0d9b5579d82",
+    "url": "/new-recipe-app/index.html"
+  },
+  {
+    "revision": "046aa3b15e33e25f45d7",
+    "url": "/new-recipe-app/static/css/main.cef14bc8.chunk.css"
+  },
+  {
+    "revision": "0966ac700cb4e3dacf55",
+    "url": "/new-recipe-app/static/js/2.b9b418f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/new-recipe-app/static/js/2.b9b418f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "046aa3b15e33e25f45d7",
+    "url": "/new-recipe-app/static/js/main.6abd5595.chunk.js"
+  },
+  {
+    "revision": "cfc0069ad2cfd8a821a0",
+    "url": "/new-recipe-app/static/js/runtime-main.2d6f2e65.js"
+  },
+  {
+    "revision": "7cd92069f47b48e50dedd69f7a704558",
+    "url": "/new-recipe-app/static/media/Illustration1.7cd92069.png"
+  },
+  {
+    "revision": "f38341afb2b3bf63ca799902e06e90df",
+    "url": "/new-recipe-app/static/media/Img1.f38341af.jpg"
+  }
+]);
